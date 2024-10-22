739e0a58ae95ff0251b0b728d599e011e152d4db
Graham Johnston <graham.s.johnston@outlook.com>
2024-10-22 11:52:03 -0400

    updated decoder

--- Decoder_Function.py
+++ Decoder_Function.py
@@ -1,6 +1,13 @@
 # Graham Johnston
 
 def decoder(password):
-    decoded_password = ''.join(str((int(d) - 3) % 10) for d in password)
+    decoded_password = ''
 
-    return decoded_password
+    for d in password:
+        deco_digit = int(d) - 3
+        if deco_digit <= 0:
+            deco_digit += 10
+
+        decoded_password += str(deco_digit)
+
+    print(f'The encoded password is {password}, and the original password is {decoded_password}.')
